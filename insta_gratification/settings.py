3673f2faf9a8298aedaf197cae8579b652ffda0f
livlockyeardev <livlockyear@gmail.com>
2026-03-02 12:28:58 +0000

    add database connection

--- insta_gratification/settings.py
+++ insta_gratification/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True').lower() == 'true'
 
