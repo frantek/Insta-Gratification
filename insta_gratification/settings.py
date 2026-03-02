bb1ac05c49ef02ef1bd6dfabdea814eb18a9e6e7
livlockyeardev <livlockyear@gmail.com>
2026-03-02 11:56:04 +0000

    link up database with package requirements

--- insta_gratification/settings.py
+++ insta_gratification/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
-
 import env
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,10 +78,7 @@ WSGI_APPLICATION = 'insta_gratification.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
